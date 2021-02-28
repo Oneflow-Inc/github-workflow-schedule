591ee1aedabf6039588d5ce1181d11464736ddba
tsai <caishenghang@oneflow.org>
2021-02-28 21:12:45 +0800

    build

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ module.exports =
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const { Octokit } = __nccwpck_require__(622);
+const { assert } = __nccwpck_require__(82);
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
 const owner = 'Oneflow-Inc';
@@ -35,14 +36,12 @@ const num_in_progress_runs = async function (status) {
             repo: repo,
             run_id: wr.id
         });
-        r.data.jobs.map(j => console.log(wr.id, "/", wr.name, "/", j.name, "/", j.status))
+        r.data.jobs.map(j => console.log(wr.status, wr.id, "/", wr.name, "/", j.name, "/", j.status))
         jobs_in_progress = r.data.jobs.filter(j => is_gpu_job(j) && j.status == "in_progress")
-        jobs_all_queued = r.data.jobs.every(j => is_gpu_job(j) && j.status == "queued")
-        schedule_job = r.data.jobs.find(j => j.name == "wait_for_gpu_slot")
+        jobs_all_queued = r.data.jobs.filter(j => is_gpu_job(j)).every(j => j.status == "queued")
+        schedule_job = r.data.jobs.find(j => j.name == "Wait for GPU slots")
+        assert(schedule_job)
         const has_passed_scheduler = (schedule_job && schedule_job.status == "completed") && jobs_all_queued
-        if (has_passed_scheduler) {
-            console.log(wr.id, "/", wr.name, "/", "queued")
-        }
         return has_passed_scheduler || jobs_in_progress.length > 0;
     })
     return (await Promise.all(promises)).filter(is_running => is_running).length
@@ -58,7 +57,8 @@ async function start() {
         console.log("trying", i + 1, "/", max_try)
         num = 100000
         try {
-            num_list = await Promise.all([num_in_progress_runs("queued"), num_in_progress_runs("in_progress")])
+            num_list = await Promise.all([num_in_progress_runs("in_progress"), num_in_progress_runs("queued")])
+            console.log("in-progress", num_list[0], "in-queue", num_list[1])
             num = num_list.reduce((a, b) => a + b, 0)
         } catch (error) {
             console.log(error)
@@ -3478,6 +3478,14 @@ function wrappy (fn, cb) {
 
 module.exports = eval("require")("encoding");
 
+
+/***/ }),
+
+/***/ 82:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("console");;
 
 /***/ }),
 
