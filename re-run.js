cb7b9a8d3c5ef2f64f1a1fd5aa0de1dd7f1e21ec
tsai <caishenghang@oneflow.org>
2021-03-19 19:50:03 +0800

    refine

--- re-run.js
+++ re-run.js
@@ -19,29 +19,24 @@ async function reRun() {
         owner: owner,
         repo: repo,
         workflow_id: test_workflow_id,
-        per_page: 20
+        per_page: 20,
+        page: 2,
     }).then(
         r => {
             Promise.all(
                 r.data.workflow_runs.map(async wr => {
+                    console.log("[processing]", wr.html_url)
                     await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
                         owner: owner,
                         repo: repo,
                         run_id: wr.id
                     }).then(
                         async r => {
-                            isNetworkFail = false
-                            r.data.jobs.map(
+                            const isNetworkFail = r.data.jobs.map(
                                 j => j.steps.map(
-                                    s => {
-                                        if (s.name.includes("checkout") || s.name.includes("Set up job")) {
-                                            if (s.status == 'completed' && s.conclusion == 'failure') {
-                                                isNetworkFail = true
-                                            }
-                                        }
-                                    }
-                                )
-                            )
+                                    s => ((s.name.includes("checkout") || s.name == 'Set up job') && s.conclusion == 'failure')
+                                ).filter(x => x == true).length > 0
+                            ).filter(x => x == true).length > 0
                             var shaSeenBefore = new Set();
                             isPrUpdatedAndOpen = false
                             isLatestCommitInBranch = false
@@ -61,34 +56,27 @@ async function reRun() {
                                     isLatestCommitInBranch = true
                                     console.log(`[absent: ${wr.head_branch}]`, wr.html_url)
                                 })
-                            await Promise.all(
-                                wr.pull_requests.map(async pr => {
-                                    base_sha = pr.base.sha
-                                    await octokit.request('GET /repos/{owner}/{repo}/compare/{base}...{head}', {
-                                        owner: owner,
-                                        repo: repo,
-                                        base: base_sha,
-                                        head: wr.head_sha
-                                    }).then(async r => {
-                                        if (r.data.behind_by == 0) {
-                                            await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
-                                                owner: owner,
-                                                repo: repo,
-                                                pull_number: pr.number
-                                            }).then(r => {
-                                                if (r.data.state == "open") {
-                                                    console.log("[pr updated]", wr.html_url)
-                                                    isPrUpdatedAndOpen = true
-                                                } else {
-                                                    console.log("[pr closed]", wr.html_url)
-                                                }
-                                            })
-                                        } else {
-                                            console.log("[pr behind base]", wr.html_url)
-                                        }
-                                    })
+                            isPrUpdatedAndOpen = await wr.pull_requests.reduce(async (acc, pr) => {
+                                base_sha = pr.base.sha
+                                return await octokit.request('GET /repos/{owner}/{repo}/compare/{base}...{head}', {
+                                    owner: owner,
+                                    repo: repo,
+                                    base: base_sha,
+                                    head: wr.head_sha
+                                }).then(async r => {
+                                    isPrOpen = false
+                                    if (r.data.behind_by == 0) {
+                                        isPrOpen = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+                                            owner: owner,
+                                            repo: repo,
+                                            pull_number: pr.number
+                                        }).then(r => r.data.state == "open")
+                                    } else {
+                                        console.log("[pr behind base]", wr.html_url)
+                                    }
+                                    return (isPrOpen && r.data.behind_by == 0) || acc
                                 })
-                            )
+                            }, false)
 
                             shouldReRun = isPrUpdatedAndOpen && isNetworkFail && isLatestCommitInBranch
                             if (shouldReRun) {
@@ -99,7 +87,9 @@ async function reRun() {
                                     run_id: wr.id
                                 }).then(r => console.log(console.log(`[rerun: ${r.status}]`, wr.html_url)))
                             }
+                            isShaSeenBefore = false
                             if (shaSeenBefore.has(wr.head_sha)) {
+                                isShaSeenBefore = true
                                 console.log("[duplicated]", wr.html_url)
                             }
                             if (['in_progress', 'queued'].includes(wr.status)) {
@@ -132,11 +122,18 @@ async function reRun() {
                                                 reviewers: [
                                                     'oneflow-ci-bot'
                                                 ]
-                                            })
+                                            }).catch(e => console.log("[failed to add reviewer]", wr.html_url))
                                         }
                                     })
                                 )
                             }
+                            console.log(wr.id, {
+                                isPrUpdatedAndOpen: isPrUpdatedAndOpen,
+                                isNetworkFail: isNetworkFail,
+                                isLatestCommitInBranch: isLatestCommitInBranch,
+                                relatedPRSize: wr.pull_requests.length,
+                                isShaSeenBefore: isShaSeenBefore
+                            })
                             shaSeenBefore.add(wr.head_sha)
                         }
                     )
